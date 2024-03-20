04058ae107156f5db5ab78e6be93b866c669135a
Ian Wise <ixwise0318@gmail.com>
2024-03-20 17:26:30 -0500

    PolygonJava

---
+++ PolygonJava/src/main/java/com/polygon/model/base/Triangle.java
@@ -0,0 +1,28 @@
+package com.polygon.model.base;
+
+public abstract class Triangle extends Polygon {
+    private final double sideA;
+    private final double sideB;
+    private final double sideC;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        super(3);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+
+}
